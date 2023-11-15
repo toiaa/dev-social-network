b816282e0a99d0f87a0aa929f3085851d3e73087
toiaa <victoria@geddes.com.ar>
2023-11-15 12:48:35 +0100

    adding auth and routes

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold flex-center">Next.js 13 we&apos;re coming</h1>{" "}
-      <h2 className="h2-bold flex-center">Next.js 13 we&apos;re coming</h2>
-      <h3 className="h3-bold flex-center">Next.js 13 we&apos;re coming</h3>
-    </div>
-  );
-}
