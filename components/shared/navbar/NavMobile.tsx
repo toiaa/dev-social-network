6f947205023b9b526be3a5aefd5ef0e6dda8d59c
toiaa <victoria@geddes.com.ar>
2023-11-17 22:34:52 +0100

    navbar mobile styling with routes and sheet

--- components/shared/navbar/NavMobile.tsx
+++ components/shared/navbar/NavMobile.tsx
@@ -1,20 +1,52 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Link from "next/link";
 import { SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 
 const NavContent = () => {
-  return <h1>navContent</h1>;
+  const pathname = usePathname();
+  return (
+    <section className="flex h-full flex-col gap-6 p-16">
+      {sidebarLinks.map((item) => {
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+        return (
+          <SheetClose asChild key={item.route}>
+            <Link
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : " text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4 `}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
 };
 const MobileNav = () => {
   return (
@@ -30,7 +62,7 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent
         side="left"
-        className="background-light900_dark200 border-nones"
+        className="background-light900_dark200 border-none"
       >
         <Link href="/" className="flex items-center gap-1">
           <Image
@@ -51,8 +83,15 @@ const MobileNav = () => {
             <div className="flex flex-col gap-3">
               <SheetClose asChild>
                 <Link href="/sign-in">
-                  <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3">
-                    <span className="primary-text-gradient">LogIn</span>
+                  <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">Sign In</span>
+                  </Button>
+                </Link>
+              </SheetClose>{" "}
+              <SheetClose asChild>
+                <Link href="/sign-up">
+                  <Button className="small-medium light-border-2 btn-tertiary  text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    Sign Up
                   </Button>
                 </Link>
               </SheetClose>
