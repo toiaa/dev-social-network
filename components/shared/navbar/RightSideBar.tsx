3e49e84930c409748f99f43ce84d2d9f7644205a
toiaa <victoria@geddes.com.ar>
2023-11-30 17:19:33 +0100

    fixed some styles and changed a function to arrow

--- components/shared/navbar/RightSideBar.tsx
+++ components/shared/navbar/RightSideBar.tsx
@@ -19,7 +19,7 @@ function RightSideBar() {
     { _id: 5, name: 'typescript', totalQuestions: 5 },
   ]
   return (
-    <section className='background-light900_dark200 light-border custom-scrollbar fixed inset-y-0 right-0 flex min-h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[350px]'>
+    <section className='custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden'>
       <div className=''>
         <h3 className='text-dark200_light900 h3-bold'>Top Questions</h3>
         <div className='mt-7 flex w-full flex-col gap-[30px]'>
