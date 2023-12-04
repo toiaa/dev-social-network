fcb7d8c72c3813183dba8942ce7c3e439e88a7bc
toiaa <victoria@geddes.com.ar>
2023-12-04 18:50:15 +0100

    tag input functionality added

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -9,9 +9,11 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from '@/components/ui/input'
 import { questionSchema } from '@/lib/validations'
 import { Editor } from '@tinymce/tinymce-react'
+import { Badge } from '@/components/ui/badge'
+import Image from 'next/image'
 
 export function Question() {
-  const editorRef = useRef(null)
+  const editorRef = useRef({})
   const form = useForm<z.infer<typeof questionSchema>>({
     resolver: zodResolver(questionSchema),
     defaultValues: {
@@ -23,6 +25,32 @@ export function Question() {
 
   const onSubmit = (values: z.infer<typeof questionSchema>) => {
     console.log(values)
+  }
+  const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, field: any) => {
+    if (e.key === 'Enter' && field.name === 'tags') {
+      e.preventDefault()
+      const tagInput = e.target as HTMLInputElement
+      const tagValue = tagInput.value.trim()
+      if (tagValue !== '') {
+        if (tagValue.length > 15) {
+          return form.setError('tags', {
+            type: 'required',
+            message: 'Tag length must be less than 15 characters',
+          })
+        }
+        if (!field.value.includes(tagValue) as never) {
+          form.setValue('tags', [...field.value, tagValue])
+          tagInput.value = ''
+          form.clearErrors('tags')
+        }
+      } else {
+        form.trigger()
+      }
+    }
+  }
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag)
+    form.setValue('tags', newTags)
   }
   return (
     <>
@@ -109,9 +137,34 @@ export function Question() {
                   <Input
                     placeholder='e.g. react, javascript, css'
                     className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border  '
-                    {...field}
+                    onKeyDown={(e) => {
+                      handleInputKeyDown(e, field)
+                    }}
                   />
                 </FormControl>
+                {field.value.length > 0 && (
+                  <div className='flex-start mt-3.5 gap-2.5'>
+                    {field.value.map((tag: any) => {
+                      return (
+                        <Badge
+                          key={tag}
+                          className='subtle-medium text-dark400_light500 background-light800_dark300 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize'>
+                          {tag}
+                          <Image
+                            alt='Close icon'
+                            className='cursor-pointer object-contain invert-0 dark:invert'
+                            src='/assets/icons/close.svg'
+                            width={12}
+                            height={12}
+                            onClick={() => {
+                              handleTagRemove(tag, field)
+                            }}
+                          />
+                        </Badge>
+                      )
+                    })}
+                  </div>
+                )}
                 <FormDescription className='body-regular mt-2.5 text-light-500'>
                   Add up to 5 tags to describe what your question is about
                 </FormDescription>
