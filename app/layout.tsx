2de8f54977d3fd46ad063b5a264465e34609b195
toiaa <victoria@geddes.com.ar>
2023-12-19 12:03:27 +0100

    lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,42 @@
-import { ThemeProvider } from "@/context/ThemeProvider";
-import { ClerkProvider } from "@clerk/nextjs";
-import type { Metadata } from "next";
-import { Inter, Space_Grotesk } from "next/font/google";
-import React from "react";
-import "./globals.css";
+import { ThemeProvider } from '@/context/ThemeProvider'
+import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import React from 'react'
+import './globals.css'
 
 const inter = Inter({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-inter",
-});
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
 const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-spaceGrotesk",
-});
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+})
 export const metadata: Metadata = {
-  title: "DevFlow",
-  description:
-    "DevFlow is a platform for developers to share their knowledge and experience with the community.",
+  title: 'DevFlow',
+  description: 'DevFlow is a platform for developers to share their knowledge and experience with the community.',
   icons: {
-    icon: "/assets/images/site-logo.svg",
+    icon: '/assets/images/site-logo.svg',
   },
-};
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
         <ClerkProvider
           appearance={{
             elements: {
-              formButtonPrimary: "primary-gradient",
-              footerActionLink: "primary-text-gradient hover:text-primary-500",
+              formButtonPrimary: 'primary-gradient',
+              footerActionLink: 'primary-text-gradient hover:text-primary-500',
             },
-          }}
-        >
+          }}>
           <ThemeProvider>{children}</ThemeProvider>
         </ClerkProvider>
       </body>
     </html>
-  );
+  )
 }
